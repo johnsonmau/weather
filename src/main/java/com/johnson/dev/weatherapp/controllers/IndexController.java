f9e1d95ca41c34c3245d18a3cbb4c1f6844f4c13
Maurice Johnson <maurice.johnson9911@gmail.com>
2021-10-20 14:48:51 -0400

    moved controllers out/logic into service

--- src/main/java/com/johnson/dev/weatherapp/controllers/IndexController.java
+++ src/main/java/com/johnson/dev/weatherapp/controllers/IndexController.java
@@ -1,139 +1,36 @@
 package com.johnson.dev.weatherapp.controllers;
 
-import com.johnson.dev.weatherapp.models.*;
-import com.johnson.dev.weatherapp.services.ForecastService;
+import com.johnson.dev.weatherapp.services.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
-
 
 @Controller
 public class IndexController {
 
     @Autowired
-    private ForecastService forecastService;
-
-    @GetMapping("/privacy")
-    public String getPP(){
-        return "privacy";
-    }
-
-    @GetMapping("/terms")
-    public String getTerms(){
-        return "terms";
-    }
+    private IndexService indexService;
 
     @GetMapping("/")
     public String getIndex(){
-        return "redirect:/home";
+        return indexService.getIndex();
     }
 
     @GetMapping("/home")
-    public String getIndex(HttpServletRequest request, HttpServletResponse response,
+    public String getIndex2(){
+        return indexService.getIndex();
+    }
+
+    @GetMapping("/home/{unit}")
+    public String getIndex(HttpServletRequest request, HttpServletResponse response, @PathVariable String unit,
                            @RequestParam(required = false) String location, Model model) {
-
-        int locationSize = getRecentLocations(request).size();
-
-        List<String> lastThreeLocNames = getRecentLocations(request);
-        if (locationSize > 0) {
-            model.addAttribute("lastThreeLocs", lastThreeLocNames);
-        }
-
-        LocationAndForecast forecastAndLoc = null;
-        if (location != null) {
-
-            if (location.trim() != "") {
-                model.addAttribute("inputLocation", location);
-
-                forecastAndLoc = forecastService.getForecastAndLocation(location);
-            }
-
-        }
-        if (forecastAndLoc != null) {
-
-            GeocodedLocation geocodedLocation = forecastAndLoc.getLocation();
-            List<Feature> features = geocodedLocation.getFeatures();
-            Feature feature = features.get(0);
-            String placeName = feature.getPlaceName();
-            Double longitude = feature.getCoordinates().get(0);
-            Double latitude = feature.getCoordinates().get(1);
-
-            model.addAttribute("longitude", longitude);
-            model.addAttribute("latitude", latitude);
-
-            String cookieName = "location" + locationSize;
-            String cookieValue = placeName
-                    .replaceAll(", ", "commaspace").replaceAll(" ", "space");
-
-            addRecentLocation(request, response, cookieName, cookieValue);
-
-            model.addAttribute("location", placeName);
-
-            Forecast forecast = forecastAndLoc.getForecast();
-            CurrentForecast currentForecast = forecast.getCurrent();
-            List<DailyForecast> dailyForecasts = forecast.getDaily();
-            List<HourForecast> hourlyForecasts = forecast.getHourly();
-            model.addAttribute("dailyForecasts", dailyForecasts);
-            model.addAttribute("currentForecast", currentForecast);
-            model.addAttribute("hourlyForecast", hourlyForecasts);
-
-        } else {
-            if (location != null) model.addAttribute("error", "Cannot get weather data for the entered location.");
-        }
-        return "index";
+        return indexService.getIndex(request, response, unit, location, model);
     }
 
-    public void addRecentLocation(HttpServletRequest request, HttpServletResponse response, String cookieName,
-                                  String cookieValue){
-        boolean cookieExists = false;
-
-        if (request.getCookies() != null) {
-            List<Cookie> cookieList = Arrays.asList(request.getCookies());
-
-            for (Cookie cook : cookieList) {
-                if (cook.getValue().equals(cookieValue)) {
-                    cookieExists = true;
-                    break;
-                }
-            }
-
-            if (cookieExists == false) {
-                Cookie locationCookie = new Cookie(cookieName, cookieValue);
-                response.addCookie(locationCookie);
-            }
-        } else {
-            Cookie locationCookie = new Cookie(cookieName, cookieValue);
-            response.addCookie(locationCookie);
-        }
-    }
-
-    public List<String> getRecentLocations(HttpServletRequest request) {
-        Cookie[] existingCookies = request.getCookies();
-        List<String> lastThreeLocNames = new ArrayList<>(3);
-
-        if (existingCookies != null) {
-            int j;
-
-            if (existingCookies.length < 3) j = existingCookies.length;
-            else j = 3;
-
-            for (int i = existingCookies.length - 1; i >= 0; i--) {
-                if (existingCookies[i].getName().contains("location")) {
-                    String recentLocNameModified = existingCookies[i].getValue()
-                            .replaceAll("commaspace", ", ").replace("space", " ");
-                    lastThreeLocNames.add(recentLocNameModified);
-                    if (lastThreeLocNames.size() == 3) break;
-                }
-            }
-        }
-        return lastThreeLocNames;
-    }
 
 }
